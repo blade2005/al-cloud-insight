b25ee683d0573818e2c3d9646749b450467ce98d
Craig Davis <cdavis@alertlogic.com>
2017-07-12 10:31:13 -0500

    Adding some debugging for authentication

--- al_cloud_insight/__init__.py
+++ al_cloud_insight/__init__.py
@@ -1,13 +1,18 @@
+import logging
 from hammock import Hammock
 
 from tacoma import Tacoma
 from cloud_explorer import CloudExplorer
 from assets import Assets
+
 class CloudInsight(object):
     def __init__(self, username, password, host='https://api.cloudinsight.alertlogic.com', version='v1'):
         # We'll do our auth here so we have a working cloud insight hammock object
         pre_auth_ci = Hammock(host, auth=(username, password))
-        resp = pre_auth_ci.aims(version).authenticate.POST().json()
+        resp = pre_auth_ci.aims(version).authenticate.POST()
+        logging.debugging(resp.status_code)
+        resp.raise_for_status()
+        resp = resp.json()
         self.account_id = resp['authentication']['account']['id']
         self.auth_token = resp['authentication']['token']
         self.version = version
